c016e7bef1a6ead3875be0c5f888f7120437ab3b
WBRK-dev <koertjrweberp@gmail.com>
2023-01-23 17:40:32 +0100

    Fixed wrong location

--- betaProgram.js
+++ betaProgram.js
@@ -1,4 +1,4 @@
-$("head").append('<link rel="stylesheet" href="/betaProgram.css">')
+$("head").append('<link rel="stylesheet" href="/wbrkgames/betaProgram.css">')
 betaPopup = '<div class="betaPopup"><div class="popup"><h2>This is the beta program.</h2><a href="https://wbrkgames.netlify.app">Goto Release</a><a onclick="betaRemove()">Close</a></div></div>';
 $("body").append(betaPopup);
 function betaRemove() {$(".betaPopup").remove()};
